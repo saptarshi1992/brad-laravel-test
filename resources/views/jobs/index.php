84c2641fc79cd5b7066e0e7343fddd2375a5b2ae
saptarshi1992 <saptarshi34.sc@gmail.com>
2025-02-11 18:36:40 +0530

    add jobs

---
+++ resources/views/jobs/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jobs View:</title>
+</head>
+<body>
+    <h1>Available Jobs</h1>
+    <ul>
+        <li>Software Enginner</li>
+        <li>DBA</li>
+        <li>Technical Manager</li>
+        <li>Systems Admin</li>
+    </ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -6,48 +6,56 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/jobs', function () {
-    return "<h1> NO Jobs </h1>";
+    return view('jobs.index');
 })->name('jobs');
-Route::get('/test', function () {
-    $url = route('jobs');
-    return '<a href=' . $url . '>click</a>';
-});
-// Route::match(
-//     ['get', 'post'],
-//     '/submit',
-//     function () {
-//         return "submitted";
-//     }
-// );
-Route::get('api/user', function () {
-    return ['name' => 'john doe', 'email' => 'john@gmail.com'];
-});
-Route::get('/user-data/{id}/comments/{comment_id}', function (string $id, string $commnet_id) {
-    return "user-id::" . $id . " comment-id::" . $commnet_id;
-});
-Route::get('/use', function (Request $request) {
-    return [
-        'method' => $request->method(),
-        'url' => $request->url(),
-        'path' => $request->path(),
-        'fullUrl' => $request->fullUrl(),
-        'ip' => $request->ip(),
-        'userAgent' => $request->userAgent(),
-        'header' => $request->header()
-    ];
-});
-Route::get('/user-data', function (Request $request) {
-    return $request->all();
-});
-Route::get('/error', function () {
-    return response("<h1>error</h1>", 404);
-});
-Route
-    ::get
-    ('/user-new', function () {
-        return response()->json(['name' => 'saptarshi'])->cookie('name','saptarshi');
-    });
-Route::get('/get-cookie', function (Request $request) {
-    $cookieValue = $request->cookie('name');
-    return response()->json(['name' => $cookieValue]);
-});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+})->name('jobs');
+
+
+
+// for testing:
+// Route::get('/test', function () {
+//     $url = route('jobs');
+//     return '<a href=' . $url . '>click</a>';
+// });
+// // Route::match(
+// //     ['get', 'post'],
+// //     '/submit',
+// //     function () {
+// //         return "submitted";
+// //     }
+// // );
+// Route::get('api/user', function () {
+//     return ['name' => 'john doe', 'email' => 'john@gmail.com'];
+// });
+// Route::get('/user-data/{id}/comments/{comment_id}', function (string $id, string $commnet_id) {
+//     return "user-id::" . $id . " comment-id::" . $commnet_id;
+// });
+// Route::get('/use', function (Request $request) {
+//     return [
+//         'method' => $request->method(),
+//         'url' => $request->url(),
+//         'path' => $request->path(),
+//         'fullUrl' => $request->fullUrl(),
+//         'ip' => $request->ip(),
+//         'userAgent' => $request->userAgent(),
+//         'header' => $request->header()
+//     ];
+// });
+// Route::get('/user-data', function (Request $request) {
+//     return $request->all();
+// });
+// Route::get('/error', function () {
+//     return response("<h1>error</h1>", 404);
+// });
+// Route
+//     ::get
+//     ('/user-new', function () {
+//         return response()->json(['name' => 'saptarshi'])->cookie('name','saptarshi');
+//     });
+// Route::get('/get-cookie', function (Request $request) {
+//     $cookieValue = $request->cookie('name');
+//     return response()->json(['name' => $cookieValue]);
+// });
