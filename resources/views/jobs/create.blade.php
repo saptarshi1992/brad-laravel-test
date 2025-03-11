bf27b8a09d5575b805fa9cd88a99ae9b0857b378
saptarshi1992 <saptarshi34.sc@gmail.com>
2025-03-11 18:28:11 +0530

    work on create job

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -10,90 +10,27 @@
                 <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
                     Job Info
                 </h2>
-               <x-text id="title" name="title" label="Job-Title" placeholder="Software Engineer"/>
-                {{-- <div class="mb-4">
-                    <label class="block text-gray-700" for="title">Job Title</label>
-                    <input id="title" type="text" name="title"
-                        class="w-full px-4 py-2 border rounded focus:outline-none @error('title') border-red-500 @enderror"
-                        placeholder="Software Engineer" value={{ old('title') }}>
-                    @error('title')
-                        <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
-                    @enderror
-                </div> --}}
 
-                <div class="mb-4">
-                    <label class="block text-gray-700" for="description">Job Description</label>
-                    <textarea cols="30" rows="7" id="description" name="description"
-                        class="w-full px-4 py-2 border rounded focus:outline-none @error('description') border-red-500 @enderror"
-                        placeholder="We are seeking a skilled and motivated Software Developer to join our growing development team..."
-                        value={{ old('description') }}></textarea>
-                    @error('description')
-                        <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-                
-                <x-text id="salary" name="salary" label="Annual Salary" type="number" placeholder="123456"/>
-
-                {{-- <div class="mb-4">
-                    <label class="block text-gray-700" for="salary">Annual Salary</label>
-                    <input id="salary" type="number" name="salary"
-                        class="w-full px-4 py-2 border rounded focus:outline-none" placeholder="90000"
-                        value={{ old('salary') }}>
-                    @error('salary')
-                        <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
-                    @enderror
-                </div> --}}
-
-                <div class="mb-4">
-                    <label class="block text-gray-700" for="requirements">Requirements</label>
-                    <textarea id="requirements" name="requirements" class="w-full px-4 py-2 border rounded focus:outline-none"
-                        placeholder="Bachelor's degree in Computer Science"></textarea>
-                </div>
-
-                <div class="mb-4">
-                    <label class="block text-gray-700" for="benefits">Benefits</label>
-                    <textarea id="benefits" name="benefits" class="w-full px-4 py-2 border rounded focus:outline-none"
-                        placeholder="Health insurance, 401k, paid time off"></textarea>
-                </div>
-
-                {{-- <div class="mb-4">
-                    <label class="block text-gray-700" for="tags">Tags (comma-separated)</label>
-                    <input id="tags" type="text" name="tags"
-                        class="w-full px-4 py-2 border rounded focus:outline-none"
-                        placeholder="development,coding,java,python" />
-                </div> --}}
-                <x-text id="tags" name="tags" label="Tags (comma-separated)"  placeholder="development, coding, java, python"/>
-
-                <div class="mb-4">
-                    <label class="block text-gray-700" for="job_type">Job Type</label>
-                    <select id="job_type" name="job_type" class="w-full px-4 py-2 border rounded focus:outline-none @error('description') border-red-500 @enderror">
-                        <option value="Full-Time" {{ old('job_type') == 'Full-Time' ? 'selected' : '' }}>
-                            Full-Time
-                        </option>
-                        <option value="Part-Time" {{ old('job_type') == 'Part-Time' ? 'selected' : '' }}>
-                            Part-Time
-                        </option>
-                        <option value="Contract" {{ old('job_type') == 'Contract' ? 'selected' : '' }}>
-                            Contract
-                        </option>
-                        <option value="Temporary" {{ old('job_type') == 'Temporary' ? 'selected' : '' }}>
-                            Temporary
-                        </option>
-                        <option value="Internship" {{ old('job_type') == 'Internship' ? 'selected' : '' }}>
-                            Internship
-                        </option>
-                        <option value="Volunteer" {{ old('job_type') == 'Volunteer' ? 'selected' : '' }}>
-                            Volunteer
-                        </option>
-                        <option value="On-Call" {{ old('job_type') == 'On-Call' ? 'selected' : '' }}>
-                            On-Call
-                        </option>
-                    </select>
-                    @error('job_type')
-                        <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
+                <x-text id="title" name="title" label="Job-Title" placeholder="Software Engineer" />
+                <x-text-area id="description" name="description" label="Job Description"
+                    placeholder="We are seeking a skilled and motivated Software Developer to join our growing development team..." />
+                <x-text id="salary" name="salary" label="Annual Salary" type="number" placeholder="123456" />
+                <x-text-area id="requirements" name="requirements" label="Requirements"
+                    placeholder="Bachelor's degree in Computer Science" />
+                <x-text-area id="benefits" name="benefits" label="Benefits"
+                    placeholder="Health insurance, 401k, paid time off" />
+                <x-text id="tags" name="tags" label="Tags (comma-separated)"
+                    placeholder="development, coding, java, python" />
+                <x-select id="job_type" name="job_type" label="Job Type" :options="[
+                    'Full-Time' => 'Full-Time',
+                    'Part-Time' => 'Part-Time',
+                    'Contract' => 'Contract',
+                    'Temporary' => 'Temporary',
+                    'Internship' => 'Internship',
+                    'Volunteer' => 'Volunteer',
+                    'On-Call' => 'On-Call',
+                ]"
+                    value="{{ old('job_type') }}" />
                 <div class="mb-4">
                     <label class="block text-gray-700" for="remote">Remote</label>
                     <select id="remote" name="remote" class="w-full px-4 py-2 border rounded focus:outline-none">
@@ -102,57 +39,25 @@
                     </select>
                 </div>
 
-                {{-- <div class="mb-4">
-                    <label class="block text-gray-700" for="address">Address</label>
-                    <input id="address" type="text" name="address"
-                        class="w-full px-4 py-2 border rounded focus:outline-none" placeholder="123 Main St" />
-                </div> --}}
-                <x-text id="address" name="address" label="Address" placeholder="123 Main St"/>
-                <x-text id="city" name="city" label="City" placeholder="Kolkata"/>
-                <x-text id="state" name="state" label="State" placeholder="WB"/>
-                <x-text id="zipcode" name="zipcode" label="ZIP Code" placeholder="700008"/>
-                
+                <x-text id="address" name="address" label="Address" placeholder="123 Main St" />
+                <x-text id="city" name="city" label="City" placeholder="Kolkata" />
+                <x-text id="state" name="state" label="State" placeholder="WB" />
+                <x-text id="zipcode" name="zipcode" label="ZIP Code" placeholder="700008" />
+
                 <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
                     Company Info
                 </h2>
-                <x-text id="address" name="address" label="Address" placeholder="123 Main St"/>
-                {{-- <div class="mb-4">
-                    <label class="block text-gray-700" for="company_name">Company Name</label>
-                    <input id="company_name" type="text" name="company_name"
-                        class="w-full px-4 py-2 border rounded focus:outline-none" placeholder="Company name" />
-                </div> --}}
 
-                <div class="mb-4">
-                    <label class="block text-gray-700" for="company_description">Company Description</label>
-                    <textarea id="company_description" name="company_description"
-                        class="w-full px-4 py-2 border rounded focus:outline-none" placeholder="Company Description"></textarea>
-                </div>
-
-                <div class="mb-4">
-                    <label class="block text-gray-700" for="company_website">Company Website</label>
-                    <input id="company_website" type="text" name="company_website"
-                        class="w-full px-4 py-2 border rounded focus:outline-none" placeholder="Enter website" />
-                </div>
-
-                <div class="mb-4">
-                    <label class="block text-gray-700" for="contact_phone">Contact Phone</label>
-                    <input id="contact_phone" type="text" name="contact_phone"
-                        class="w-full px-4 py-2 border rounded focus:outline-none" placeholder="Enter phone" />
-                </div>
-
-                <div class="mb-4">
-                    <label class="block text-gray-700" for="contact_email">Contact Email</label>
-                    <input id="contact_email" type="email" name="contact_email"
-                        class="w-full px-4 py-2 border rounded focus:outline-none"
-                        placeholder="Email where you want to receive applications" />
-                </div>
-
-                <div class="mb-4">
-                    <label class="block text-gray-700" for="company_logo">Company Logo</label>
-                    <input id="company_logo" type="file" name="company_logo"
-                        class="w-full px-4 py-2 border rounded focus:outline-none" />
-                </div>
-
+                <x-text id="address" name="address" label="Address" placeholder="123 Main St" />
+                <x-text id="company_name" name="company_name" label="Company Name" placeholder="Company Name" />
+                <x-text-area id="company_description" name="company_description" label="Company Description"
+                    placeholder="Company Description" />
+                <x-text id="company_website" name="company_website" label="Company Website"
+                    placeholder="Enter Website" />
+                <x-text id="contact_phone" name="contact_phone" label="Contact Phone" placeholder="Enter Phone" />
+                <x-text id="contact_email" name="contact_email" label="Contact Email" type='email'
+                    placeholder="Email where you want to receive applications" />
+                <x-text id="company_logo" name="company_logo" label="Company Logo" type='file' />
                 <button type="submit"
                     class="w-full bg-green-500 hover:bg-green-600 text-white px-4 py-2 my-3 rounded focus:outline-none">
                     Save
